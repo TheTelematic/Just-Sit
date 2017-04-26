37889738e9e304c1b8ed96697230064df3fc2191
Arturo Jesus Parra Soto <arturoteleco95@protonmail.ch>
2017-04-26 13:42:17 +0200

    He cambiado el TextView

--- app/src/main/java/es/uma/artjuan/just_sit/MainActivity.java
+++ app/src/main/java/es/uma/artjuan/just_sit/MainActivity.java
@@ -2,12 +2,19 @@ package es.uma.artjuan.just_sit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tx = (TextView) findViewById(R.id.texto);
+
+        tx.setText("Hola juanca");
     }
 }
